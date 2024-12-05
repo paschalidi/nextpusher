e69e7559dd084b58a65d1fe60241d31744e657c7
Christos Paschalidis <paschalidi@outlook.com>
2024-12-05 22:02:20 +0000

    clean is good

--- lib/pusher.ts
+++ lib/pusher.ts
@@ -1,7 +1,7 @@
 import PusherServer from 'pusher';
 
 export const pusherServer = new PusherServer({
-  key: process.env.PUSHER_KEY!,
+  key: process.env.NEXT_PUBLIC_PUSHER_KEY!,
   secret: process.env.PUSHER_SECRET!,
   appId: process.env.NEXT_PUBLIC_PUSHER_APP_ID!,
   cluster: process.env.NEXT_PUBLIC_PUSHER_CLUSTER!,
